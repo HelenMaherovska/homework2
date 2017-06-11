563c2188cb72e1e2a2ca4009c01495755a0bc543
hmaherovska <maher.olena96@gmail.com>
2017-06-11 20:00:58 +0300

    fixed code according to comments

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -5,24 +5,26 @@ module StateAndBehaviour
   # define class Car
   class Car
     attr_accessor :year, :color, :model, :current_speed
+
     def self.default_car
       Car.new {}
     end
 
     def initialize(car_attributes)
       raise ArgumentError, 'Expects hash' unless car_attributes.is_a?(Hash)
-      self.year = car_attributes.fetch(:year, 2015)
-      self.color = car_attributes.fetch(:color, 'purple')
-      self.model = car_attributes.fetch(:model, 'skyline')
-      self.current_speed = 0
+
+      @year = car_attributes.fetch(:year, 2015)
+      @color = car_attributes.fetch(:color, 'purple')
+      @model = car_attributes.fetch(:model, 'skyline')
+      @current_speed = 0
     end
 
     def speed_up(speed)
-      self.current_speed = speed
+      @current_speed = speed
     end
 
     def push_break(speed)
-      self.current_speed -= speed if speed <= current_speed
+      @current_speed -= speed if speed <= current_speed
     end
   end
 end
