f35400946b57a200becb2df168a147bc1f94af4f
hmaherovska <maher.olena96@gmail.com>
2017-06-11 14:59:31 +0300

    implemented object_model.rb

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,24 @@
 
 # exercise object model
 module ObjectModel
+  # define molule LinuxFriendly
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+  # define parent class for Laptop
+  class Computer
+    def mine_bitcoins
+      inspect
+    end
+
+    def fork_process
+      'Parent: allocate memory'
+    end
+  end
+  # define child class Laptop
+  class Laptop < Computer
+    prepend LinuxFriendly
+  end
 end
