6766f4b1ae8c82cea05f503e07145b3e19897215
hmaherovska <maher.olena96@gmail.com>
2017-06-12 16:07:59 +0300

    changed attr_accessor to attr_reader and renamed argument speed in class Car

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -4,7 +4,7 @@
 module StateAndBehaviour
   # define class Car
   class Car
-    attr_accessor :year, :color, :model, :current_speed
+    attr_reader :year, :color, :model, :current_speed
 
     def self.default_car
       Car.new {}
@@ -19,12 +19,12 @@ module StateAndBehaviour
       @current_speed = 0
     end
 
-    def speed_up(speed)
-      @current_speed += speed
+    def speed_up(more_speed)
+      @current_speed += more_speed
     end
 
-    def push_break(speed)
-      @current_speed -= speed if speed <= current_speed
+    def push_break(more_speed)
+      @current_speed -= more_speed if more_speed <= current_speed
     end
   end
 end
